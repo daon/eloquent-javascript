c36f597942b18cf7bda2a72b5ee3b41f7c41345c
Dan Andersson <dan.andersson@outlook.com>
2016-11-10 21:41:29 +0000

    Added FizzBuzz

---
+++ 02-program-structure/02-fizz-buzz.js
@@ -0,0 +1,22 @@
+function fizzBuzz(n) {
+    var str = '';
+    if (n % 3 === 0) {
+        str += 'Fizz';
+    } 
+    
+    if (n % 5 === 0) {
+        str += 'Buzz';
+    }
+    cc
+    return !!str ? str : n;
+}
+
+function range(from, to, action) {
+    for (var i=from;i<=to;i++) {
+        action(i);
+    }
+};
+
+range(1, 100, function(n) {
+    console.log(fizzBuzz(n));
+});
