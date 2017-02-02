bd2aa604eb4140d2398e5ad557f3d9525f1507f6
Dan Andersson <dan.andersson@outlook.com>
2017-02-02 17:38:18 +0000

    Finished exercise A vector type

--- 06-the-secret-life-of-objects/01-a-vector-type.js
+++ 06-the-secret-life-of-objects/01-a-vector-type.js
@@ -15,6 +15,24 @@
 
 */
 // Your code here.
+function Vector(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+Vector.prototype.plus = function(other) {
+    return new Vector(this.x + other.x, this.y + other.y);
+}
+
+Vector.prototype.minus = function(other) {
+    return new Vector(this.x - other.x, this.y - other.y);
+}
+
+Object.defineProperty(Vector.prototype, 'length', {
+    get: function() {
+        return Math.sqrt(Math.pow(this.x, 2)+Math.pow(this.y, 2));
+    }
+})
 
 console.log(new Vector(1, 2).plus(new Vector(2, 3)));
 // → Vector{x: 3, y: 5}
@@ -22,3 +40,26 @@ console.log(new Vector(1, 2).minus(new Vector(2, 3)));
 // → Vector{x: -1, y: -1}
 console.log(new Vector(3, 4).length);
 // → 5
+
+// Factory function
+var vectorProto = {
+    plus(other) {
+        return createVector(this.x + other.x, this.y + other.y);
+    },
+    minus(other) {
+        return createVector(this.x - other.x, this.y - other.y);
+    },
+    get length() {
+        return Math.sqrt(Math.pow(this.x, 2)+Math.pow(this.y, 2));
+    }
+}
+
+var createVector = (x, y) => Object.assign(Object.create(vectorProto), { x, y }); 
+
+console.log(createVector(1, 2).plus(createVector(2, 3)));
+// → Vector{x: 3, y: 5}
+console.log(createVector(1, 2).minus(createVector(2, 3)));
+// → Vector{x: -1, y: -1}
+console.log(createVector(3, 4).length);
+// → 5
+
